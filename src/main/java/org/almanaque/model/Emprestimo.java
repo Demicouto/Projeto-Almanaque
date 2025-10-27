ec0fd71f6550436bfb7c782d67cc189148e15e28
Demétrio Coutinho <demetriorc8@gmail.com>
2025-10-27 10:19:08 -0300

    feat(DB): Implementação das tabelas emprestimo e livro.

--- src/main/java/org/almanaque/model/Emprestimo.java
+++ src/main/java/org/almanaque/model/Emprestimo.java
@@ -6,7 +6,7 @@ public class Emprestimo {
 
     private int id;
     private int usuarioId;
-    private int livroIsbn;
+    private i nt livroIsbn;
     private LocalDateTime dataEmprestimo;
     private LocalDateTime dataDevolucao;
     private StatusEmprestimo status;
--- src/main/java/org/almanaque/util/DatabaseSetup.java
+++ src/main/java/org/almanaque/util/DatabaseSetup.java
@@ -15,11 +15,42 @@ public class DatabaseSetup {
                 tipo VARCHAR(100) NOT NULL
             );
             """;
+
+        String sqlLivro = """
+            CREATE TABLE IF NOT EXISTS livros (
+                isbn VARCHAR(13) PRIMARY KEY,
+                titulo VARCHAR(255) NOT NULL,
+                autor VARCHAR(255) NOT NULL,
+                ano INT,
+                quantidadeTotal INT NOT NULL,
+                quantidadeDisponivel INT NOT NULL,
+                categoria VARCHAR(100)
+            );
+            """;
+
+        String sqlEmprestimo = """
+            CREATE TABLE IF NOT EXISTS emprestimos (
+                id SERIAL PRIMARY KEY,
+                usuarioId INT NOT NULL REFERENCES usuarios(id),
+                livro_isbn VARCHAR(13) NOT NULL REFERENCES livros(isbn),
+                dataEmprestimo TIMESTAMP NOT NULL,
+                dataDevolucao TIMESTAMP,
+                status VARCHAR(50) NOT NULL,
+                multa DECIMAL(10, 2) DEFAULT 0.0
+            );
+            """;
+
         try (Connection conn = DataBaseConnection.getConnection();
              Statement stmt = conn.createStatement()) {
 
             stmt.execute(sqlUsuario);
             System.out.println("Tabela 'usuario' criada ou já existe!");
+
+            stmt.execute(sqlLivro);
+            System.out.println("Tabela 'livro' criado com sucesso ou ja existente!");
+
+            stmt.execute(sqlEmprestimo);
+            System.out.println("Tabela 'emprestimos' criado com sucesso ou ja existente!");
 
         } catch (SQLException e) {
             e.printStackTrace();
